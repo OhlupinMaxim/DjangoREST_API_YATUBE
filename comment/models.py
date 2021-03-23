8ec1c5cd849500022b3fd37df30c65ff4a7fcc42
Максим Охлупин <maximohlupin.omd@yandex.ru>
2021-03-23 23:18:45 +0300

    create models COMMENT REVIEW

--- comment/models.py
+++ comment/models.py
@@ -1,5 +1,18 @@
 from django.db import models
 
+from review.models import Review
+
+from user.models import User
+
 
 class Comment(models.Model):
-    pass
+    review = models.ForeignKey(Review,
+                               on_delete=models.CASCADE,
+                               related_name="comment_review")
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               related_name="comment_author")
+    pub_date = models.DateTimeField("Дата Публикации",
+                                    auto_now_add=True,
+                                    db_index=True)
+    text = models.TextField
--- review/models.py
+++ review/models.py
@@ -1,4 +1,27 @@
+from django.core.validators import MinValueValidator
+from django.core.validators import MaxValueValidator
 from django.db import models
 
-class Review(models.Model):
+from user.models import User
+
+
+class Title(models.Model):
     pass
+
+
+class Review(models.Model):
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               related_name="review_author")
+    title = models.ForeignKey(Title,
+                              on_delete=models.CASCADE,
+                              related_name="review_title")
+    pub_date = models.DateTimeField("Дата Публикации",
+                                    auto_now_add=True,
+                                    db_index=True)
+    score = models.IntegerField(default=1,
+                                validators=[
+                                    MaxValueValidator(10),
+                                    MinValueValidator(1)
+                                ])
+    text = models.TextField
