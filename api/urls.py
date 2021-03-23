c99ab30351d74476f89f8c80793163b2a6b2abd5
Максим Охлупин <maximohlupin.omd@yandex.ru>
2021-03-24 00:25:26 +0300

    add url for Reviews || Comments  --- viewSets is NOT COMPLETED

--- api/urls.py
+++ api/urls.py
@@ -2,10 +2,25 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from .views import UserViewSet
+from .views import ReviewViewSet
+from .views import CommentViewSet
 
 router = DefaultRouter()
 
 router.register('users', UserViewSet, 'users')
+
+"""
+    урлы для других моделей туточки
+"""
+
+router.register(
+    "r'titles/(?P<title_id>\d+)/reviews'",
+    ReviewViewSet,
+    "reviews")
+router.register(
+    "r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments'",
+    CommentViewSet,
+    "comments")
 
 urlpatterns = [
     path('', include(router.urls)),
--- api/views.py
+++ api/views.py
@@ -1,25 +1,33 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import filters, status, viewsets
 from rest_framework.decorators import action
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from user.models import User
 from user.permissions import IsAdmin
+from user.permissions import IsAuthorOrAdminOrModerator
+
+from review.models import Review
+
+from comment.models import Comment
 
 from .serializers import UserSerializer, YamdbRoleSerializer
+from .serializers import CommentSerializer
+from .serializers import ReviewSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
     lookup_field = 'username'
-    permission_classes = (IsAuthenticated, IsAdmin, )
-    filter_backends = (filters.SearchFilter, )
+    permission_classes = (IsAuthenticated, IsAdmin,)
+    filter_backends = (filters.SearchFilter,)
     search_fields = 'username'
 
     @action(methods=['get', 'patch'], detail=False,
-            permission_classes=(IsAuthenticated, ),
+            permission_classes=(IsAuthenticated,),
             url_path='me')
     def user_profile(self, request):
         user = get_object_or_404(User, id=request.user.id)
@@ -31,3 +39,44 @@ class UserViewSet(viewsets.ModelViewSet):
             serializer.is_valid(raise_exception=True)
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    pagination_class = PageNumberPagination
+    permission_classes = [IsAuthorOrAdminOrModerator, ]
+
+    def get_queryset(self):
+        title_id = self.kwargs.get("title_id")
+        pass
+
+    def perform_create(self, serializer):
+        title_id = self.kwargs.get("title_id")
+        pass
+
+    def partial_update(self, request, *args, **kwargs):
+        title_id = self.kwargs.get("title_id")
+        pass
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    pagination_class = PageNumberPagination
+    permission_classes = [IsAuthorOrAdminOrModerator, ]
+
+    def get_queryset(self):
+        title_id = self.kwargs.get("title_id")
+        review_id = self.kwargs.get("review_id")
+        pass
+
+    def perform_create(self, serializer):
+        title_id = self.kwargs.get("title_id")
+        review_id = self.kwargs.get("review_id")
+        pass
+
+    def partial_update(self, request, *args, **kwargs):
+        title_id = self.kwargs.get("title_id")
+        review_id = self.kwargs.get("review_id")
+        pass
