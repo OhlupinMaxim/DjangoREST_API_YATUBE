3625cbd7a7c157fe2c4b635b8a72a06debff7111
Максим Охлупин <maximohlupin.omd@yandex.ru>
2021-03-28 13:59:18 +0300

    Review v2

--- review/models.py
+++ review/models.py
@@ -7,27 +7,44 @@ from user.models import User
 
 
 class Review(models.Model):
-    author = models.ForeignKey(User,
-                               on_delete=models.CASCADE,
-                               related_name='review_author',
-                               blank=True,
-                               null=True)
-    title = models.ForeignKey(Title,
-                              on_delete=models.CASCADE,
-                              related_name='review_title',
-                              blank=True,
-                              null=True)
-    pub_date = models.DateTimeField('Дата Публикации',
-                                    auto_now_add=True,
-                                    db_index=True)
-    score = models.IntegerField(default=1,
-                                validators=[
-                                    MaxValueValidator(10),
-                                    MinValueValidator(1)
-                                ])
-    text = models.TextField()
+    """
+    Модель Review(Отзыв).
+    Поле author(Автор) внешний ключ на модель User(Пользователь).
+    Поле title(Произведение) внешний ключ на модель Title(Произведение).
+    Поле pub_date(Дата публикации), cоздается автоматически.
+    Поле score(Оценка), оценка на произведение.
+    Поле text(Текст Отзыва).
+    """
+    author = models.ForeignKey(
+        User,
+        verbose_name='Автор Отзыва',
+        on_delete=models.CASCADE,
+        related_name='review_author',
+        blank=True,
+        null=True)
+    title = models.ForeignKey(
+        Title,
+        verbose_name='Произведение',
+        on_delete=models.CASCADE,
+        related_name='review_title',
+        blank=True,
+        null=True)
+    pub_date = models.DateTimeField(
+        verbose_name='Дата Публикации',
+        auto_now_add=True,
+        db_index=True)
+    score = models.IntegerField(
+        verbose_name='Оценка',
+        default=1,
+        validators=[
+            MaxValueValidator(10),
+            MinValueValidator(1)
+        ])
+    text = models.TextField(verbose_name='Текст отзыва')
 
     class Meta:
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
         ordering = ['pub_date']
 
     def __str__(self):
